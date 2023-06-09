3e3da38ef3afdbde5419b3abe3fa66af7562c512
ronaldkasaija <reenez.den@gmail.com>
2023-06-08 20:43:03 -0500

    Added new files

---
+++ src/main/java/com/project/weather_rtdis/WeatherRtdisApplication.java
@@ -0,0 +1,13 @@
+package com.project.weather_rtdis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherRtdisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeatherRtdisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/weather_rtdis/WeatherRtdisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.weather_rtdis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherRtdisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
